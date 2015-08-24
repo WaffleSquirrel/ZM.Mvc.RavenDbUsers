682980ac97c113882750fe1e47f6ba3bb294db5a
WaffleSquirrel <rockabilly@outlook.com>
2015-08-23 23:04:03 -0700

    Remove commented out code in Startup.Auth.cs and organize usings.

--- src/Website/App_Start/Startup.Auth.cs
+++ src/Website/App_Start/Startup.Auth.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
-using Raven.Client;
-using Raven.Client.Embedded;
 using ZM.Mvc.RavenDbUsers.Infrastructure.UserIdentity;
 using ZM.Mvc.RavenDbUsers.Models;
 
@@ -13,31 +11,10 @@ namespace ZM.Mvc.RavenDbUsers
 {
     public partial class Startup
     {
-        //#region Fields
-
-        //private static IDocumentStore _userDbContext;
-
-        //#endregion
-
         #region Methods
 
         public void ConfigureAuth(IAppBuilder app)
         {
-            //if (documentStore == null)
-            //{
-            //    throw new ArgumentNullException("documentStore");
-            //}
-
-            //_userDbContext = documentStore;
-
-            //// Configure per-request lifetime instances in the Owin Context for:
-            //// 1.) Application User DB Context
-            //// 2.) Application User Manager
-            //// 3.) Application Sign-In Manager
-            //app.CreatePerOwinContext<EmbeddableDocumentStore>(() => _userDbContext as EmbeddableDocumentStore);
-            //app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            //app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
-
             // Configure the application to use a cookie to store information for the signed in user.
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
@@ -62,17 +39,6 @@ namespace ZM.Mvc.RavenDbUsers
 
         static Startup()
         {
-            //OAuthOptions = new OAuthAuthorizationServerOptions
-            //{
-            //    // Exposes Token endpoint
-            //    TokenEndpointPath = new PathString("/Token"),
-            //
-            //    // Use ApplicationOAuthProvider in order to authenticate
-            //    Provider = new ApplicationOAuthProvider(PublicClientId, UserManagerFactory),
-            //               AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-            //               AccessTokenExpireTimeSpan = TimeSpan.FromDays(14), // Token expiration => The user will remain authenticated for 14 days
-            //               AllowInsecureHttp = true
-            //};
         }
 
         #endregion
